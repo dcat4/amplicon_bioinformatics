434396e5815d778db9fae7322049bf3c9f24c0a9
Kevin Son <kevinson@umail.ucsb.edu>
2020-04-17 00:19:06 -0700

    initial commit

---
+++ taxonomy_pipeline/consensus_taxonomies/consensus_tax_mostCom.R
@@ -0,0 +1,4 @@
+consensus_tax_bestRez <- function(..., tablenames = c("bayes", "idtax"), ranknamez = c("Kingdom", "Supergroup", "Division","Class","Order","Family","Genus","Species"),
+                                  tiebreakz = "LCAlike") {
+  x <- list(...)
+}
