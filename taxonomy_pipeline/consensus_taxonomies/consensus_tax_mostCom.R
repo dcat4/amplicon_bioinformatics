e9461059cb4c0c2765b3b1f2f59ea9b6c6548ade
Kevin Son <kevinson@umail.ucsb.edu>
2020-04-21 09:59:23 -0700

    draft of consensus alg

--- taxonomy_pipeline/consensus_taxonomies/consensus_tax_mostCom.R
+++ taxonomy_pipeline/consensus_taxonomies/consensus_tax_mostCom.R
@@ -1,6 +1,48 @@
-consensus_tax_bestRez <- function(..., tablenames = c("bayes", "idtax"), ranknamez = c("Kingdom", "Supergroup", "Division","Class","Order","Family","Genus","Species"),
+consensus_tax_mostCom <- function(..., tablenames = c("bayes", "idtax"), ranknamez = c("Kingdom", "Supergroup", "Division","Class","Order","Family","Genus","Species"),
                                   tiebreakz = "LCAlike") {
   x <- list(...)
+  consensus.tax <- data.frame(matrix(ncol=(ncol(x[[1]])), nrow=0, dimnames=list(NULL, names(x[[1]]))))
+  n.rows <- nrow(x[[1]])
+  n.cols <- ncol(x[[1]])
+  threshold <- 0.5
+  n.dfs <- length(x)
+  ties <- vector()
+  
+  for (row in 1:n.rows) {
+    placed <- FALSE
+    for (col in rev(3:n.cols)) {
+      taxs <- vector()
+      for (df in x) {
+        taxs <- c(taxs, df[row, col])
+      }
+      freq.df <- as.data.frame(table(taxs), stringsAsFactors=FALSE)
+      if (nrow(freq.df) > 0) {
+        freq.df$prop <- freq.df$Freq / length(taxs[!is.na(taxs)])
+        max.prop <- max(freq.df$prop)
+        if (max.prop >= threshold) {
+          c.tax <- freq.df[which(freq.df$prop == max.prop), "taxs"]
+          if (length(c.tax) > 1) {
+            ties <- c(ties, row)
+            placed <- TRUE
+            break
+          }
+          else {
+            df <- x[[match(c.tax, taxs)]]
+            result <- rbind(result, df[row, ])
+            placed <- TRUE
+            break
+          }
+        }
+      }
+    }
+    if(!placed) {
+      ties <- c(ties, row)
+    }
+  }
+  
+  return(list(result, ties))
+  
+  # NOTES
   # majority rule system
   # use the assignment that is in agreement in the most taxonomy tables
   # row and rank wise basis majority rule system if the majority of the input taxonomy
