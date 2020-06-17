a8cc8de81cf85a19b02332801945b85e1a22b140
Default Seadas User <seadas-user@localhost>
2020-06-17 07:33:50 -0700

    v9 composition analysis

--- mock_analysis/analyze_mock_v9_composition.R
+++ mock_analysis/analyze_mock_v9_composition.R
@@ -1,5 +1,34 @@
 # this script analyzes the composition of the mock v9 data set I put together, look at the number of ASVs from different taxonomic groups
 # and how variable the resolution of the expected assignments are.
 
-# UNDER CONSTRUCTION
+rm(list=ls())
+setwd("~/Documents/R/amplicon_bioinformatics/mock_analysis/")
 
+library("stringr")
+library("ggplot2")
+
+et <- readRDS("mock_data/v9_mock_exptax_bothPrimers.rds")
+et[et == "NaN"] <- NA # because this didn't happen automatically...
+
+# count NA's:
+nact <- apply(et, 1, FUN = function(x) sum(is.na(x)))
+
+hist(nact)
+
+ii <- 4
+dd <- as.data.frame(table(et[,ii]))
+ggplot(dd, aes(x="", y=Freq, fill=Var1)) + 
+  geom_bar(width=1, stat="identity") +
+  coord_polar("y") +
+  ggtitle(paste0("Unique ",colnames(et)[ii])) +
+  theme_void() +
+  theme(plot.title = element_text(hjust=0.5), legend.title=element_blank())
+
+ii <- 5
+dd <- as.data.frame(table(et[,ii]))
+ggplot(dd, aes(x="", y=Freq, fill=Var1)) + 
+  geom_bar(width=1, stat="identity") +
+  coord_polar("y") +
+  ggtitle(paste0("Unique ",colnames(et)[ii])) +
+  theme_void() +
+  theme(plot.title = element_text(hjust=0.5), legend.title=element_blank())
